6c2b87409861ac093e95d5702ef4048892471a3b
Hotblueice777 <joeyqi777@gmail.com>
2024-12-19 21:39:14 -0700

    fix: recover the part of original code in Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,13 +10,13 @@ export const Header = () => {
   return (
     <>
       <header className="box-border flex h-[84px] items-center justify-between border-b border-[#E1E1E1] px-[9px]">
-        <Link href="/" className="mr-[56px] uppercase">
+        <Link href="/" className="mr-[56px] whitespace-nowrap text-[32px] uppercase">
           Go Alberta
         </Link>
         <div className="flex gap-[50px]">
           <NavLinks />
         </div>
-        <div className="ml-auto flex gap-[50px]">
+        <div className="ml-auto flex gap-[50px] text-[20px]">
           <span>Start a Campaign</span>
           <button onClick={() => setShowLoginModal(true)} className="cursor-pointer hover:underline">
             Log in / Sign up
