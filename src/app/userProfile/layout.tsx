cb2b0eab7317b3a28fce0f39033e2fdd635c20c1
Hotblueice777 <joeyqi777@gmail.com>
2024-12-24 21:26:20 -0700

    change SideBar width

--- src/app/userProfile/layout.tsx
+++ src/app/userProfile/layout.tsx
@@ -4,19 +4,15 @@ import SideBar from '@/components/UserProfile/SideBar'
 export default function UserProfileLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex min-h-screen">
-      {/* 左侧边栏 - 固定宽度 */}
-      <div className="min-h-screen w-64 border-r">
+      <div className="min-h-screen w-1/6 border-r">
         <SideBar />
       </div>
 
-      {/* 右侧内容区域 */}
       <div className="flex flex-1 flex-col">
-        {/* 顶部导航栏 - 固定高度 */}
         <div className="h-16 border-b">
           <NavBar />
         </div>
 
-        {/* 主要内容区域 - 自适应高度 */}
         <main className="flex-1 p-6">{children}</main>
       </div>
     </div>
